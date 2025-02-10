252f2a9b6d06dc953f57424500ed8ba900d8d281
Seth Arvila <setharvila@hotmail.com>
2025-02-10 12:32:22 -0600

    Updated README.md, updated tests for greater code coverage

--- ChallengeTests/TimeCheckViewModelTests.swift
+++ ChallengeTests/TimeCheckViewModelTests.swift
@@ -105,7 +105,6 @@ class TimeCheckViewModelTests: XCTestCase {
         XCTAssertTrue(viewModel.isWithinOfficeHours())
     }
     
-    
     func testBoolForIsWithinOfficeHours_ReturnTrueExactEnd() {
         var components = DateComponents()
         components.day = 10
@@ -139,4 +138,78 @@ class TimeCheckViewModelTests: XCTestCase {
         
         XCTAssertFalse(viewModel.isWithinOfficeHours())
     }
+    
+    func testStringforGetMessageInOfficeHours() {
+        var components = DateComponents()
+        components.day = 10
+        components.month = 2
+        components.year = 2025
+        components.hour = 10
+        components.minute = 30
+        components.second = 0
+       
+        let inputDate = Calendar.current.date(from: components)! // Feb 10, 2025 10:30:00 AM
+        
+        let officeHours = OfficeHours(daysOfWeek: [1], startTime: "10:00", endTime: "11:00")
+        let viewModel = TimeCheckViewModel(date: inputDate, officeHours: officeHours)
+        let expectedString = "Office hours are avaliable during this time"
+        _ = viewModel.isWithinOfficeHours()
+        XCTAssertEqual(expectedString, viewModel.getMessage())
+    }
+    
+    func testStringforGetMessageOutOfOfficeHours() {
+        var components = DateComponents()
+        components.day = 11
+        components.month = 2
+        components.year = 2025
+        components.hour = 11
+        components.minute = 30
+        components.second = 0
+       
+        let inputDate = Calendar.current.date(from: components)! // Feb 10, 2025 11:30:00 AM
+        
+        let officeHours = OfficeHours(daysOfWeek: [1], startTime: "10:00", endTime: "11:00")
+        let viewModel = TimeCheckViewModel(date: inputDate, officeHours: officeHours)
+        let expectedString = "Sorry, office hours are not avaliable during this time"
+        _ = viewModel.isWithinOfficeHours()
+        XCTAssertEqual(expectedString, viewModel.getMessage())
+    }
+    
+    func testSymbolforGetMessageInOfficeHours() {
+        var components = DateComponents()
+        components.day = 10
+        components.month = 2
+        components.year = 2025
+        components.hour = 10
+        components.minute = 30
+        components.second = 0
+       
+        let inputDate = Calendar.current.date(from: components)! // Feb 10, 2025 10:30:00 AM
+        
+        let officeHours = OfficeHours(daysOfWeek: [1], startTime: "10:00", endTime: "11:00")
+        let viewModel = TimeCheckViewModel(date: inputDate, officeHours: officeHours)
+        let expectedString = "checkmark.circle.fill"
+        _ = viewModel.isWithinOfficeHours()
+        XCTAssertEqual(expectedString, viewModel.getSymbol())
+    }
+    
+    func testSymbolforGetMessageOutOfOfficeHours() {
+        var components = DateComponents()
+        components.day = 11
+        components.month = 2
+        components.year = 2025
+        components.hour = 11
+        components.minute = 30
+        components.second = 0
+       
+        let inputDate = Calendar.current.date(from: components)! // Feb 10, 2025 11:30:00 AM
+        
+        let officeHours = OfficeHours(daysOfWeek: [1], startTime: "10:00", endTime: "11:00")
+        let viewModel = TimeCheckViewModel(date: inputDate, officeHours: officeHours)
+        let expectedString = "x.circle.fill"
+        _ = viewModel.isWithinOfficeHours()
+        XCTAssertEqual(expectedString, viewModel.getSymbol())
+    }
+    
+    
 }
